1b807cf6a7bcd8426321f83b4b9817609618567c
jali5859 <relishedarc@gmail.com>
2023-10-14 19:37:08 -0400

    www

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
-import requests
 from flask import Flask, send_file
+import requests
 
 app = Flask(__name__)
 
@@ -31,13 +31,64 @@ def remove_channels_without_programs(url):
     
     return modified_file_path
 
+def download_m3u_file():
+    url = 'http://url.com'
+    filename = 'tv_original.m3u'
+    response = requests.get(url)
+    with open(filename, 'w') as f:
+        f.write(response.text)
+
 @app.route('/epg')
 def serve_epg():
-    url = 'http://epg.url.com'
+    url = 'https://epgurl.com'
     modified_file_path = remove_channels_without_programs(url)
-    
     # Serve the modified EPG file
     return send_file(modified_file_path, as_attachment=True)
 
+@app.route('/tv')
+def generate_tv_m3u():
+    download_m3u_file()
+    serve_epg()
+    m3u_file = 'tv_original.m3u'
+    epg_file = 'EPG_modified.xml'
+    
+    # Load the m3u file
+    with open(m3u_file, 'r') as f:
+        m3u_data = f.readlines()
+
+    # Load the EPG.xml file
+    tree = ET.parse(epg_file)
+    root = tree.getroot()
+
+    # Extract the channel names from the EPG.xml file
+    epg_channels = set()
+    for programme in root.iter('programme'):
+        channel_id = programme.attrib['channel']
+        epg_channels.add(channel_id)
+
+    # Filter the channels in the m3u file
+    filtered_channels = []
+    flag = False
+    for line in m3u_data:
+        if line.strip() == '#EXTM3U':
+            filtered_channels.append(line)
+        elif line.startswith('#EXTINF'):
+            flag = True
+            channel_id = line.split('tvg-id="')[1].split('"')[0].strip()
+            if channel_id in epg_channels:
+                filtered_channels.append(line)
+            else:
+                flag = False
+        else:
+            if flag:
+                filtered_channels.append(line)
+
+    # Write the filtered channels to a new m3u file
+    with open('tv_new.m3u', 'w') as f:
+        for line in filtered_channels:
+            f.write(line)
+
+    return send_file('tv_new.m3u', as_attachment=True)
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5500)
+    app.run(host='0.0.0.0',port=5500)
